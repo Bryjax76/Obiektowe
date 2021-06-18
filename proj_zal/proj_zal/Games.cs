e1ed003527bcf0adcefd6e14e378957d94d9c026
Bryjax76 <66446836+Bryjax76@users.noreply.github.com>
2021-06-18 20:11:17 +0200

    Add files via upload

---
+++ proj_zal/proj_zal/Games.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper.Configuration.Attributes;
+
+namespace proj_zal
+{
+    [Table("tSteamChart")]
+    public class Games
+    {
+        [Ignore]
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Name("<gamename>")]
+        public string gamename { get; set; }
+
+        [Name("<year>")]
+        public int year { get; set; }
+
+        [Name("<month>")]
+        public string month { get; set; }
+
+        [Name("<avg>")]
+        public decimal avg { get; set; }
+
+        [Name("<gain>")]
+        public string gain { get; set; }
+
+        [Name("<peak>")]
+        public int peak { get; set; }
+
+        [Name("<avg_peak_perc>")]
+        public string avg_peak_perc { get; set; }
+    }
+}
